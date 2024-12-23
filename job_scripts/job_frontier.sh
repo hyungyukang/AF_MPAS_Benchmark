adbbae63238a64fb37d29602cc751e62cae8732d
Hyun Kang <kangh@ornl.gov>
2024-12-23 13:23:19 -0500

    Update CMakeLists and job scripts

--- job_scripts/job_frontier.sh
+++ job_scripts/job_frontier.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #SBATCH -A YourProject
 #SBATCH -J TEST_MPASA
-#SBATCH -o %x-%j.out
-#SBATCH -t 0:30:00
+#SBATCH -N 20
+#SBATCH -t 1:00:00
 #SBATCH -p batch
-#SBATCH -q debug
-#SBATCH -N 10
 #SBATCH --exclusive
 ##SBATCH --core-spec=8
 
-export OMP_NUM_THREADS=1
-srun -n 560 ./mpas_atmosphere
+srun -n 1120 ./mpas_atmosphere
