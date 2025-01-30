6f011ed8d5ea084731412d8c79fda040b2fb3417
Hyun Kang <kangh@ornl.gov>
2025-01-29 23:16:25 -0500

    Update estimate.py

--- postprocess/estimate.py
+++ postprocess/estimate.py
@@ -7,15 +7,15 @@ import os
 # Runtime (time integration) from log.ocean.0000.out (or log.ocean.00000.out)
 
 # - Runtime of VR_20km-4km_N01_D700 (<= 2400 s)
-refRuntimeVR = 2165.776
+refRuntimeVR = 2399.776
 
 # - Runtime of QU_20km
-refRuntimeQU = 1880.7826
+refRuntimeQU = 2076.7826
 
 # Number of nodes used
 nNodeUsed = 60
 
-# Number of cores per processor
+# Number of cores per node
 nCoresPerNode = 128
 
 # Number of windows (refinement regions) you want to estimate
@@ -25,18 +25,21 @@ nWindows = 10
 # Estimate #Nodes completing simulations within 40 min
 #   for increasing number of windows
 # - Linear scaling is assumed.
+#-----------------------------------------------------
 runtimeDiff = refRuntimeVR - refRuntimeQU
 
-print("----------------------------------------------------------")
-print("Runtime of VR_20km-4km_N01_D700 =",refRuntimeVR,"s")
-print("Runtime of QU_20km              =",refRuntimeQU,"s")
-print("----------------------------------------------------------")
-print("To complete the simulation within 40 mins,")
+print("--------------------------------------------------------------------")
+print("Entered runtime using",nNodeUsed,"nodes (",nNodeUsed*nCoresPerNode,"cores )")
+print(" - VR20km-1km_N01_D700 =",refRuntimeVR,"s")
+print(" - QU20km              =",refRuntimeQU,"s")
+print("--------------------------------------------------------------------")
+print("To complete the simulation within 40 minutes,")
 print(" ")
 for i in range(nWindows):
-    runtimeWithWindows = refRuntimeQU + (i+1)*runtimeDiff
+    runtimeScaler = refRuntimeVR/2400.0
+    runtimeWithWindows = refRuntimeQU * runtimeScaler + (i+1)*runtimeDiff
     nCoresEstimated = (nNodeUsed * nCoresPerNode) * runtimeWithWindows / refRuntimeVR
-    nNodesEstimated = int(nCoresEstimated / nCoresPerNode)
+    nNodesEstimated = round(nCoresEstimated / nCoresPerNode)
     print(nNodesEstimated,"nodes (", nNodesEstimated*nCoresPerNode,
           "cores ) are apporximately required for",i+1,"windows.")
-print("----------------------------------------------------------")
+print("--------------------------------------------------------------------")
